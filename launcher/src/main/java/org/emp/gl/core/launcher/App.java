294709c7441e5d78d6316182ebbe5b1eb3a41aa0
sweeffon2 <dzbillal@gmail.com>
2021-04-17 01:31:41 +0100

    DummyTimerService test avancé

--- launcher/src/main/java/org/emp/gl/core/launcher/App.java
+++ launcher/src/main/java/org/emp/gl/core/launcher/App.java
@@ -13,7 +13,7 @@ public class App {
 
     // ce code nous permettra d'enregistrer les service que notre application utilsiera 
     // lors de l'execution
-    static {        
+    static {
         Lookup.getInstance().register(TimerService.class, new DummyTimeServiceImpl());
     }
 
@@ -28,11 +28,31 @@ public class App {
         ts.addTimeChangeListener(new TimerChangeListener() {
             @Override
             public void uniteChangee(String prop, Object oldValue, Object newValue) {
-                clearScreen();
-                System.out.println("" + ts.getHeures()+":" +ts.getMinutes() + ":"
-                        +ts.getSecondes()+","+ts.getDixiemeDeSeconde());
+                //clearScreen();
+                System.out.println("" + ts.getHeures() + ":" + ts.getMinutes() + ":"
+                        + ts.getSecondes() + "," + ts.getDixiemeDeSeconde());
             }
         });
+
+        // maintenant ici nous allons créer des listeners qui vont se désaboner après un certains temps 
+        // premier listener qui se désabonne après 5 secondes !       
+        //for (int i = 0; i < 50; i++) {
+            ts.addTimeChangeListener(new TimerChangeListener() {
+                int compteArebours = (int) (Math.random() * 10) + 5;
+
+                @Override
+                public void uniteChangee(String prop, Object oldValue, Object newValue) {
+                    if (prop.equals(TimerChangeListener.SECONDE_PROP)) {
+                        compteArebours--;
+                        System.out.println("Il me reste : " + compteArebours);
+                    }
+
+                    if (compteArebours == 0) {
+                        ts.removeTimeChangeListener(this);
+                    }
+                }
+            });
+        //}
     }
 
     public static void clearScreen() {
